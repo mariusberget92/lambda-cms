d3e987b4e0601124b210a258da7d129b41048ffc
Marius Berget <mariusberget92@protonmail.com>
2026-03-20 17:50:35 +0100

    feat: wrap blocks with scoped CSS, custom attrs, font loading in BlockRenderer

--- resources/js/Components/BlockRenderer.vue
+++ resources/js/Components/BlockRenderer.vue
@@ -1,16 +1,27 @@
 <!-- resources/js/Components/BlockRenderer.vue -->
 <template>
   <div class="space-y-4">
-    <component
-      v-for="block in blocks"
-      :key="block.id"
-      :is="BLOCK_MAP[block.type]"
-      :block="block"
-    />
+    <template v-for="block in blocks" :key="block.id">
+      <component
+        v-if="block.customCss"
+        :is="'style'"
+      >#block-{{ block.id }} { {{ block.customCss }} }</component>
+      <div
+        :id="block.customId || `block-${block.id}`"
+        :class="block.customClasses || undefined"
+        :style="block.fontFamily ? { fontFamily: `'${block.fontFamily}', sans-serif` } : undefined"
+      >
+        <component
+          :is="BLOCK_MAP[block.type]"
+          :block="block"
+        />
+      </div>
+    </template>
   </div>
 </template>
 
 <script setup>
+import { onMounted, watch } from 'vue'
 import ParagraphBlock from '@/Components/Blocks/ParagraphBlock.vue'
 import HeadingBlock   from '@/Components/Blocks/HeadingBlock.vue'
 import ImageBlock     from '@/Components/Blocks/ImageBlock.vue'
@@ -22,8 +33,9 @@ import DividerBlock   from '@/Components/Blocks/DividerBlock.vue'
 import CtaBlock       from '@/Components/Blocks/CtaBlock.vue'
 import HtmlBlock      from '@/Components/Blocks/HtmlBlock.vue'
 import PostListBlock  from '@/Components/Blocks/PostListBlock.vue'
+import ContainerBlock from '@/Components/Blocks/ContainerBlock.vue'
 
-defineProps({ blocks: { type: Array, default: () => [] } })
+const props = defineProps({ blocks: { type: Array, default: () => [] } })
 
 const BLOCK_MAP = {
   paragraph: ParagraphBlock,
@@ -37,5 +49,29 @@ const BLOCK_MAP = {
   cta:       CtaBlock,
   html:      HtmlBlock,
   component: PostListBlock,
+  container: ContainerBlock,
 }
+
+const loadedFonts = new Set()
+
+function loadFont(family) {
+  if (!family || loadedFonts.has(family)) return
+  loadedFonts.add(family)
+  const link = document.createElement('link')
+  link.rel = 'stylesheet'
+  link.href = `https://fonts.googleapis.com/css2?family=${encodeURIComponent(family)}:wght@400;600;700&display=swap`
+  document.head.appendChild(link)
+}
+
+function loadFontsFromBlocks(blocks) {
+  for (const block of blocks) {
+    if (block.fontFamily) loadFont(block.fontFamily)
+    if (block.type === 'container' && block.children?.length) {
+      loadFontsFromBlocks(block.children)
+    }
+  }
+}
+
+onMounted(() => loadFontsFromBlocks(props.blocks))
+watch(() => props.blocks, (val) => loadFontsFromBlocks(val), { deep: true })
 </script>
